74b6d98bf8a3ec5227fec884b69712b90cdd0b09
Ivan Maldonado <ivanvalentinmaldonado2014@hotmail.com>
2023-06-25 13:13:42 -0300

    seccion work

--- src/components/Services.js
+++ src/components/Services.js
@@ -36,14 +36,13 @@ const Services = () => {
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row h-screen">
           <motion.div
-          variants={fadeIn("right", 0.5)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amounto: 0.3 }}
-        
-          className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
+            variants={fadeIn("right", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amounto: 0.3 }}
+          >
             <h2 className="h2 text-accent mb-6">¿Que hago?</h2>
-            <h3 className="h3 max-w-[450px] mb-16">
+            <h3 className="h3 max-w-[450px] ">
               Desde landing pages hasta aplicaciones web completamente
               responsive. Tambien Diseño Ux/Ui, diseños de logos, marketing,
               etc.
@@ -51,26 +50,35 @@ const Services = () => {
             <button className="btn btn-sm">Mirá mi trabajo</button>
           </motion.div>
 
-          <motion.div 
-          variants={fadeIn("left", 0.3)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amounto: 0.3 }}
-          
-          className="flex-1"
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amounto: 0.3 }}
+            className="flex-1 mt-12 mb-32 "
           >
             <div>
               {services.map((service, index) => {
                 const { name, description, link } = service;
                 return (
-                  <div className="border-b border-white/20 h-[146px] mb-[38px] flex" key={index}>
+                  <div
+                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
+                    key={index}
+                  >
                     <div className="max-w-[476px]">
-                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">{name}</h4>
-                      <p className="font-secondary leading-tight">{description}</p>
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {name}
+                      </h4>
+                      <p className="font-secondary leading-tight">
+                        {description}
+                      </p>
                     </div>
-                    <div className="flex flex-col flex-1 items-end"> 
-                      <a href="#" className="btn w-9 h-9 mb-[42px] flex justify-center items-center">
-                         <BsArrowUpRight/> 
+                    <div className="flex flex-col flex-1 items-end">
+                      <a
+                        href="#"
+                        className="btn w-9 h-9 mb-[42px] flex justify-center items-center"
+                      >
+                        <BsArrowUpRight />
                       </a>
                       <a href="#" className="text-gradient text-sm">
                         {link}
